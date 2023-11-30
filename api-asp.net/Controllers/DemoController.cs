4d1b0ceb35b5cb2105f498ed0994cf6d5ce84992
Shoaib <shoaibrochi9@gmail.com>
2023-11-30 19:17:44 +0600

    Add project files.

---
+++ api-asp.net/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_asp.net.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/names")]
+        public HttpResponseMessage Get()
+        {
+            var names = new string[] { "Shoaib", "Rochi" };
+            return Request.CreateResponse(HttpStatusCode.OK, names);
+        }
+        [HttpGet]
+        [Route("api/name/{st_id}")]
+        public HttpResponseMessage GetName(int st_id) //custom methos name GetName
+        {
+            string name = st_id == 1 ? "Rochi" : "Not found";
+            return Request.CreateResponse(HttpStatusCode.OK, name);
+        }
+        [HttpPost]
+        [Route("api/test/post")] //post test using postman
+        public HttpResponseMessage Post()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Post recieved");
+        }
+    }
+}
